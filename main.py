c53b1a714f5c97cd654473588683642080d4ad91
Bryan Ransil <bryan.ransil@gmail.com>
2012-02-24 05:04:27 -0800

    On the main page you can click and drag to create new rectangles. don't have the ability to reselect them... that's next!

--- main.py
+++ main.py
@@ -34,6 +34,42 @@ import createmap
 class MainHandler(webapp.RequestHandler):
     def get(self):
         self.response.out.write("<head>")
+        self.response.out.write('<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>\n')
+        self.response.out.write('<script type="text/javascript" src="/static/js/jquery/jquery.svg.js"></script>\n')
+        self.response.out.write('<script type="text/javascript">\n')
+        self.response.out.write(" var posx = -1; \n var posy = -1;\n")
+        self.response.out.write(" var pos1x = -1; \n var pos1y = -1;\n")
+        self.response.out.write(' $(document).ready(function(){\n    $(document).mousemove(function(e){ \n')
+        self.response.out.write('    posx = e.pageX;\n     posy = e.pageY;\n')
+        self.response.out.write('    });\n')
+        self.response.out.write('  });\n') 
+        self.response.out.write(" function drawShape() {\n    var shape = this.id;\n    var svg = $('#svgbasics').svg('get');\n")
+        self.response.out.write("    var x = 0; \n    var y = 0; \n    var width = 0; \n    var height = 0;\n")
+        self.response.out.write("    if (pos1x > posx) {\n      x = posx;\n      width = pos1x - posx;\n    " + 
+                                "} else { \n      x = pos1x;\n      width = posx - pos1x;\n    }\n")
+        self.response.out.write("    if (pos1y > posy) {\n      y = posy;\n      height = pos1y - posy;\n    " + 
+                                "} else { \n      y = pos1y;\n      height = posy - pos1y;\n    }\n")
+        self.response.out.write('    $("span").text("X: " + x + ", Y: " + y + ", X1: " + width + ", Y1: " + height);\n')
+        self.response.out.write("    svg.rect(x, y, width, height, {fill: 'yellow', stroke: 'navy', strokeWidth: 5});\n")
+        self.response.out.write(" }")
+        self.response.out.write(' $(document).ready(function(){\n    $("#svgbasics").mousedown(function(e) {\n')
+        self.response.out.write('      pos1x = e.pageX;\n')
+        self.response.out.write('      pos1y = e.pageY;\n')
+        self.response.out.write('    });\n')
+        self.response.out.write('  });\n') 
+        self.response.out.write(' $(function() {')
+        self.response.out.write("    $('#svgbasics').svg({onLoad: drawInitial});\n")
+        self.response.out.write('    $("#svgbasics").mouseup(drawShape);\n') 
+        self.response.out.write(" }\n );\n")
+        self.response.out.write(" function random(range) {\n    return Math.floor(Math.random() * range);\n}\n")
+        self.response.out.write(" function drawInitial(svg) {\n")
+        self.response.out.write("    svg.circle(75, 75, 50, {fill: 'none', stroke: 'red', 'stroke-width': 3});\n")
+        self.response.out.write("    var g = svg.group({stroke: 'black', 'stroke-width': 2});\n")
+        self.response.out.write("    svg.line(g, 15, 75, 135, 75);\n")
+        self.response.out.write("    svg.line(g, 75, 15, 75, 135);\n")
+        self.response.out.write(" }\n")
+        self.response.out.write(" </script>\n")
+        
         self.response.out.write("</head>")
         
         # Set the cross origin resource sharing header to allow AJAX
@@ -41,9 +77,13 @@ class MainHandler(webapp.RequestHandler):
         self.response.out.write("<body>")
         # Create a client class which will make HTTP requests with Google Docs server. 
         self.response.out.write('Welcome to the Board Game Platform\n')
+        self.response.out.write('<button id="activate">Reveal!</button>\n')
+        self.response.out.write('<span></span>\n')
+        self.response.out.write('<div id="svgbasics"></div>')
         self.response.out.write(""" <form action='oauth2callback' method='post' name ='oauth2'>
             <input type = "hidden" name = "type" value="login" />
             <input type="submit" value="Enter!"/></form> \n""")
+        self.response.out.write('<div id="svgbasics"></div>')
         self.response.out.write("</body>")
 
 		
--- static/js/basic.js
+++ static/js/basic.js
@@ -1,4 +1,4 @@
-document.write("<script type='text/javascript' src='jquery.js'></script>"); //if there are javascript problems, the site that I originally got this from said this script tag should be ...scr" + "ipt>"... but this stopped html from being loaded (therefore placing it at the end of the document allowed the HTML to be loaded)
+//if there are javascript problems, the site that I originally got this from said this script tag should be ...scr" + "ipt>"... but this stopped html from being loaded (therefore placing it at the end of the document allowed the HTML to be loaded)
 var interval = self.setInterval('content("incoming")', 1000);
 var interval2 = self.setInterval('content("incoming2")', 1000);
 function content(id) {
@@ -8,7 +8,7 @@ function content(id) {
 /*function onClick() {
   document.getElementById("theplace").innerHTML = "HI!";
 };
-*/
+
 function newShape(x, y, width, height) {
   var xmlHttp = new XMLHttpRequest();
   alert("Setting response handler");
@@ -20,4 +20,24 @@ function newShape(x, y, width, height) {
   alert("get command");
   xmlHttp.open("GET","/newrect",true);
   xmlHttp.send(null);
+}*/
+
+function circle() {
+    $("#svgfig").append('<rect id="star2" x="20" y="10" width="60" height="60" stroke="black" fill="none" />\n');
 }
+
+/*function thePaper(){
+  return paper;
+}*/
+
+/*$(document).ready(function(){
+  $("img").click(function(e){
+      var position = $("div#center").position();
+      var x = e.pageX - position.left;
+      var y = e.pageY - position.top;
+      alert("Hello!");
+      //$("span").append("X: " + x + ", Y: " + y);
+      //$("svgfig").append('<rect id="star" x="20" y="10" width="60" height="60" stroke="black" fill="none" />\n');
+      //$("svgfig").append('<rect id="star" x="20" y="10" width="60" height="60" stroke="black" fill="none" />');
+    });
+});*/
